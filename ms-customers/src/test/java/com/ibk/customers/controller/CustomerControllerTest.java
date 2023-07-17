b14fe3d59922b2ac793c81b2fd59ca51c7424d1d
Bryan Zavala <bzavalavaldivia@gmail.com>
2023-07-17 11:58:50 -0500

    feat/ms-customers: customers service and controller tests

---
+++ ms-customers/src/test/java/com/ibk/customers/controller/CustomerControllerTest.java
@@ -0,0 +1,301 @@
+package com.ibk.customers.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class CustomerControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CustomerService customerService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final List<Product> products = new ArrayList<>() {{
+        add(Product
+                .builder()
+                .id(1L)
+                .customerId(1L)
+                .type("CUENTA_AHORROS")
+                .name("Cuenta de Ahorros")
+                .balance(1000000.0)
+                .build());
+        add(Product
+                .builder()
+                .id(2L)
+                .customerId(1L)
+                .type("CUENTA_CORRIENTE")
+                .name("Cuenta Corriente")
+                .balance(500000.0)
+                .build());
+        add(Product
+                .builder()
+                .id(3L)
+                .customerId(1L)
+                .type("TARJETA_CREDITO_AMEX")
+                .name("Tarjeta de Crédito AMEX")
+                .balance(1000000.0)
+                .build());
+    }};
+
+    @Test
+    @DisplayName("Create Customer")
+    void createCustomer() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("1234567890")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("1234567890")
+                .build();
+
+        given(customerService.createCustomer(any(Customer.class)))
+                .willAnswer((invocation) -> invocation.getArgument(0));
+
+        // When
+        ResultActions result = mockMvc.perform(post("/api/v1/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customer)));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(customer.getId()))
+                .andExpect(jsonPath("$.uuid").value(customer.getUuid()))
+                .andExpect(jsonPath("$.firstName").value(customer.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customer.getLastName()))
+                .andExpect(jsonPath("$.documentType").value(customer.getDocumentType()))
+                .andExpect(jsonPath("$.documentNumber").value(customer.getDocumentNumber()))
+        ;
+    }
+
+    @Test
+    @DisplayName("Get All Customers")
+    void getAllCustomers() throws Exception {
+        // Given
+        List<Customer> customers = new ArrayList<Customer>();
+        customers.add(Customer.builder().id(1L).uuid("1234567890").firstName("John").lastName("Doe").documentType("CC").documentNumber("1234567890").build());
+        customers.add(Customer.builder().id(2L).uuid("0987654321").firstName("Jane").lastName("Doe").documentType("CC").documentNumber("0987654321").build());
+        customers.add(Customer.builder().id(3L).uuid("1234509876").firstName("John").lastName("Smith").documentType("CC").documentNumber("1234509876").build());
+
+        given(customerService.getAllCustomers()).willReturn(customers);
+
+        // When
+        ResultActions result = mockMvc.perform(get("/api/v1/customers"));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(customers.size()));
+    }
+
+    @Test
+    @DisplayName("Get Customer By Id")
+    void getCustomerById() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("1234567890")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("1234567890")
+                .build();
+
+        given(customerService.getCustomerById(any(Long.class))).willReturn(Optional.of(customer));
+
+        // When
+        ResultActions result = mockMvc.perform(get("/api/v1/customers/{id}", 1L));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(customer.getId()))
+                .andExpect(jsonPath("$.uuid").value(customer.getUuid()))
+                .andExpect(jsonPath("$.firstName").value(customer.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customer.getLastName()))
+                .andExpect(jsonPath("$.documentType").value(customer.getDocumentType()))
+                .andExpect(jsonPath("$.documentNumber").value(customer.getDocumentNumber()));
+    }
+
+    @Test
+    @DisplayName("Get Customer By Id Not Found")
+    void getCustomerByIdNotFound() throws Exception {
+        // Given
+        given(customerService.getCustomerById(1L)).willReturn(Optional.empty());
+
+        // When
+        ResultActions result = mockMvc.perform(get("/api/v1/customers/{id}", 1L));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Get Products By Customer Id")
+    void getProductsByCustomerId() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("1234567890")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("1234567890")
+                .products(products)
+                .build();
+
+        given(customerService.getProductsByCustomerUuid("123456")).willReturn(Optional.of(customer));
+
+        // When
+        ResultActions result = mockMvc.perform(get("/api/v1/customers/{uuid}/products", "123456"));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(customer.getId()))
+                .andExpect(jsonPath("$.uuid").value(customer.getUuid()))
+                .andExpect(jsonPath("$.firstName").value(customer.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customer.getLastName()))
+                .andExpect(jsonPath("$.documentType").value(customer.getDocumentType()))
+                .andExpect(jsonPath("$.documentNumber").value(customer.getDocumentNumber()))
+                .andExpect(jsonPath("$.products.size()").value(customer.getProducts().size()));
+    }
+
+    @Test
+    @DisplayName("Update Customer")
+    void updateCustomer() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("1234567890")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("1234567890")
+                .build();
+
+        Customer customerUpdated = Customer
+                .builder()
+                .id(1L)
+                .uuid("1234567890")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("DNI")
+                .documentNumber("12345678")
+                .build();
+
+        given(customerService.getCustomerById(1L)).willReturn(Optional.of(customer));
+        given(customerService.updateCustomer(any(Customer.class))).willAnswer((invocation) -> invocation.getArgument(0));
+
+        // When
+        ResultActions result = mockMvc.perform(put("/api/v1/customers/{id}", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customerUpdated)));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(customerUpdated.getId()))
+                .andExpect(jsonPath("$.uuid").value(customerUpdated.getUuid()))
+                .andExpect(jsonPath("$.firstName").value(customerUpdated.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(customerUpdated.getLastName()))
+                .andExpect(jsonPath("$.documentType").value(customerUpdated.getDocumentType()))
+                .andExpect(jsonPath("$.documentNumber").value(customerUpdated.getDocumentNumber()));
+    }
+
+    @Test
+    @DisplayName("Update Customer Not Found")
+    void updateCustomerNotFound() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("1234567890")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("1234567890")
+                .build();
+
+        given(customerService.getCustomerById(1L)).willReturn(Optional.empty());
+
+        // When
+        ResultActions result = mockMvc.perform(put("/api/v1/customers/{id}", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customer)));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    @DisplayName("Delete Customer")
+    void deleteCustomer() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("1234567890")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("1234567890")
+                .build();
+
+        given(customerService.getCustomerById(1L)).willReturn(Optional.of(customer));
+
+        // When
+        ResultActions result = mockMvc.perform(delete("/api/v1/customers/{id}", 1L));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Delete Customer Not Found")
+    void deleteCustomerNotFound() throws Exception {
+        // Given
+        given(customerService.getCustomerById(1L)).willReturn(Optional.empty());
+
+        // When
+        ResultActions result = mockMvc.perform(delete("/api/v1/customers/{id}", 1L));
+
+        // Then
+        result.andDo(print())
+                .andExpect(status().isNotFound());
+    }
+}
---
+++ ms-customers/src/test/java/com/ibk/customers/service/CustomerServiceTest.java
@@ -0,0 +1,242 @@
+package com.ibk.customers.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerServiceTest {
+    @InjectMocks
+    private CustomerService customerService;
+
+    @Mock
+    private CustomerRepository customerRepository;
+
+    private final List<Product> products = new ArrayList<>() {{
+        add(Product
+                .builder()
+                .id(1L)
+                .customerId(1L)
+                .type("CUENTA_AHORROS")
+                .name("Cuenta de Ahorros")
+                .balance(1000000.0)
+                .build());
+        add(Product
+                .builder()
+                .id(2L)
+                .customerId(1L)
+                .type("CUENTA_CORRIENTE")
+                .name("Cuenta Corriente")
+                .balance(500000.0)
+                .build());
+        add(Product
+                .builder()
+                .id(3L)
+                .customerId(1L)
+                .type("TARJETA_CREDITO_AMEX")
+                .name("Tarjeta de Crédito AMEX")
+                .balance(1000000.0)
+                .build());
+    }};
+
+    @BeforeEach
+    void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Create Customer")
+    void createCustomer() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("uuid")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("123456789")
+                .build();
+
+        given(customerRepository.save(any(Customer.class))).willReturn(customer);
+
+        // When
+        Customer savedCustomer = customerService.createCustomer(customer);
+
+        // Then
+        assertAll("Create Customer",
+                () -> assertNotNull(savedCustomer),
+                () -> assertEquals(customer.getUuid(), savedCustomer.getUuid()),
+                () -> assertEquals(customer.getFirstName(), savedCustomer.getFirstName()),
+                () -> assertEquals(customer.getLastName(), savedCustomer.getLastName()),
+                () -> assertEquals(customer.getDocumentType(), savedCustomer.getDocumentType()),
+                () -> assertEquals(customer.getDocumentNumber(), savedCustomer.getDocumentNumber())
+        );
+    }
+
+    @Test
+    @DisplayName("Get All Customers")
+    void getAllCustomers() throws Exception {
+        // Given
+        List<Customer> customers = new ArrayList<>();
+        customers.add(Customer
+                .builder()
+                .id(1L)
+                .uuid("uuid")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("123456789")
+                .build());
+
+        given(customerRepository.findAll()).willReturn(customers);
+
+        // When
+        List<Customer> returnedCustomers = customerService.getAllCustomers();
+
+        // Then
+        assertAll("Get All Customers",
+                () -> assertNotNull(returnedCustomers),
+                () -> assertEquals(customers.size(), returnedCustomers.size()),
+                () -> assertEquals(customers.get(0).getUuid(), returnedCustomers.get(0).getUuid()),
+                () -> assertEquals(customers.get(0).getFirstName(), returnedCustomers.get(0).getFirstName()),
+                () -> assertEquals(customers.get(0).getLastName(), returnedCustomers.get(0).getLastName()),
+                () -> assertEquals(customers.get(0).getDocumentType(), returnedCustomers.get(0).getDocumentType()),
+                () -> assertEquals(customers.get(0).getDocumentNumber(), returnedCustomers.get(0).getDocumentNumber())
+        );
+    }
+
+    @Test
+    @DisplayName("Get Customer By Id")
+    void getCustomerById() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("uuid")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("123456789")
+                .build();
+
+        given(customerRepository.findById(any(Long.class))).willReturn(java.util.Optional.of(customer));
+
+        // When
+        Customer returnedCustomer = customerService.getCustomerById(1L).get();
+
+        // Then
+        assertAll("Get Customer By Id",
+                () -> assertNotNull(returnedCustomer),
+                () -> assertEquals(customer.getUuid(), returnedCustomer.getUuid()),
+                () -> assertEquals(customer.getFirstName(), returnedCustomer.getFirstName()),
+                () -> assertEquals(customer.getLastName(), returnedCustomer.getLastName()),
+                () -> assertEquals(customer.getDocumentType(), returnedCustomer.getDocumentType()),
+                () -> assertEquals(customer.getDocumentNumber(), returnedCustomer.getDocumentNumber())
+        );
+    }
+
+    @Test
+    @DisplayName("Update Customer")
+    void updateCustomer() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("uuid")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("123456789")
+                .build();
+
+        given(customerRepository.save(any(Customer.class))).willReturn(customer);
+
+        // When
+        Customer savedCustomer = customerService.updateCustomer(customer);
+
+        // Then
+        assertAll("Update Customer",
+                () -> assertNotNull(savedCustomer),
+                () -> assertEquals(customer.getUuid(), savedCustomer.getUuid()),
+                () -> assertEquals(customer.getFirstName(), savedCustomer.getFirstName()),
+                () -> assertEquals(customer.getLastName(), savedCustomer.getLastName()),
+                () -> assertEquals(customer.getDocumentType(), savedCustomer.getDocumentType()),
+                () -> assertEquals(customer.getDocumentNumber(), savedCustomer.getDocumentNumber())
+        );
+    }
+
+    @Test
+    @DisplayName("Delete Customer")
+    void deleteCustomer() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("uuid")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("123456789")
+                .build();
+
+        willDoNothing().given(customerRepository).deleteById(any(Long.class));
+
+        // When
+        customerService.deleteCustomer(1L);
+
+        // Then
+        verify(customerRepository, times(1)).deleteById(any(Long.class));
+    }
+
+    @Test
+    @DisplayName("Get Products By Customer Uuid")
+    void getProductsByCustomerUuid() throws Exception {
+        // Given
+        Customer customer = Customer
+                .builder()
+                .id(1L)
+                .uuid("uuid")
+                .firstName("John")
+                .lastName("Doe")
+                .documentType("CC")
+                .documentNumber("123456789")
+                .products(products)
+                .build();
+
+        given(customerRepository.findByUuid(any(String.class))).willReturn(Optional.of(customer));
+
+        // When
+        Customer returnedCustomer = customerService.getProductsByCustomerUuid("uuid").get();
+
+        // Then
+        assertAll("Get Products By Customer Uuid",
+                () -> assertNotNull(returnedCustomer),
+                () -> assertEquals(customer.getUuid(), returnedCustomer.getUuid()),
+                () -> assertEquals(customer.getFirstName(), returnedCustomer.getFirstName()),
+                () -> assertEquals(customer.getLastName(), returnedCustomer.getLastName()),
+                () -> assertEquals(customer.getDocumentType(), returnedCustomer.getDocumentType()),
+                () -> assertEquals(customer.getDocumentNumber(), returnedCustomer.getDocumentNumber()),
+                () -> assertEquals(customer.getProducts().size(), returnedCustomer.getProducts().size()),
+                () -> assertEquals(customer.getProducts().get(0).getType(), returnedCustomer.getProducts().get(0).getType()),
+                () -> assertEquals(customer.getProducts().get(0).getName(), returnedCustomer.getProducts().get(0).getName()),
+                () -> assertEquals(customer.getProducts().get(0).getBalance(), returnedCustomer.getProducts().get(0).getBalance())
+        );
+    }
+}
