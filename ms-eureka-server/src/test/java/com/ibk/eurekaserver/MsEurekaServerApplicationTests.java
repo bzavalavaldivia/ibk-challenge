234e6af0fa7eb734f1e13e6644e1a1957105b2fc
Bryan Zavala <bzavalavaldivia@gmail.com>
2023-07-17 22:00:09 -0500

    feat: config eureka server and config clients

---
+++ ms-eureka-server/src/test/java/com/ibk/eurekaserver/MsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibk.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
